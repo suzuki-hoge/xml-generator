7965e5657a1b1f3f59e654fbe9d8d8d91e72e68d
suzuki-hoge <user.ryo@gmail.com>
2019-12-14 00:20:25 +0900

    parse cond.

--- src/main/scala/XmlParser.scala
+++ src/main/scala/XmlParser.scala
@@ -1,7 +1,17 @@
 import scala.util.parsing.combinator.JavaTokenParsers
 
 object XmlParser extends JavaTokenParsers {
+  def cond: Parser[Cond] = "<cond>" ~> key ~ method <~ "</cond>" ^^ (x => Cond(x._1, x._2))
+
   def key: Parser[Key] = "<key>" ~> identifier <~ "</key>" ^^ Key
+
+  def method: Parser[Method] = "<val>" ~> ($equals | $contains) <~ "</val>"
+
+  private def $equals: Parser[Equals] = "%equal(" ~> value <~ ")" ^^ Equals
+
+  private def $contains: Parser[Contains] = "%contains(" ~> value <~ ")" ^^ Contains
+
+  private def value: Parser[Value] = identifier ^^ Value
 
   private def identifier: Parser[String] = "[a-z_]+".r
 
--- src/test/scala/XmlParserTest.scala
+++ src/test/scala/XmlParserTest.scala
@@ -1,12 +1,19 @@
-import XmlParser.key
+import ImplicitConverters._
+import XmlParser.cond
 import org.scalatest.FunSuite
 
 import scala.util.parsing.combinator.JavaTokenParsers
 
 class XmlParserTest extends FunSuite with JavaTokenParsers {
-  test("key") {
-    assert(
-      XmlParser(key, "<key>status</key>") == Key("status")
-    )
+  test("cond") {
+    val s =
+      """<cond>
+        |    <key>status</key>
+        |    <val>%equal(ng)</val>
+        |</cond>""".stripMargin
+
+    val exp = "status" $equals "ng"
+
+    assert(XmlParser(cond, s) == exp)
   }
 }
